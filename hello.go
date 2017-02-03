31252b686dcc948f20a44c21f6d8358db9e9b221
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-02-03 14:57:04 +0900

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,27 +1,75 @@
 package guestbook
 
 import (
+	"html/template"
 	"net/http"
 
-	"io"
-
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/urlfetch"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/taskqueue"
 )
+
+type Counter struct {
+	Name  string
+	Count int
+}
 
 func init() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/worker", worker)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	hc := urlfetch.Client(c)
-	res, err := hc.Get("https://example.com" + r.URL.Path)
-	if err != nil {
+	ctx := appengine.NewContext(r)
+	if name := r.FormValue("name"); name != "" {
+		t := taskqueue.NewPOSTTask("/worker", map[string][]string{"name": {name}})
+		if _, err := taskqueue.Add(ctx, t, ""); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	q := datastore.NewQuery("Counter")
+	var counters []Counter
+	if _, err := q.GetAll(ctx, &counters); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer res.Body.Close()
-	w.Header().Add("Content-Type", res.Header.Get("Content-Type"))
-	io.Copy(w, res.Body)
+	if err := handlerTemplate.Execute(w, counters); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
+
+func worker(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	name := r.FormValue("name")
+	key := datastore.NewKey(ctx, "Counter", name, 0, nil)
+	var counter Counter
+	if err := datastore.Get(ctx, key, &counter); err == datastore.ErrNoSuchEntity {
+		counter.Name = name
+	} else if err != nil {
+		log.Errorf(ctx, "%v", err)
+		return
+	}
+	counter.Count++
+	if _, err := datastore.Put(ctx, key, &counter); err != nil {
+		log.Errorf(ctx, "%v", err)
+	}
+}
+
+var handlerTemplate = template.Must(template.New("handler").Parse(handlerHTML))
+
+const handlerHTML = `
+{{range .}}
+<p>{{.Name}}: {{.Count}}</p>
+{{end}}
+<p>Start a new counter:</p>
+<form action="/" method="POST">
+<input type="text" name="name">
+<input type="submit" value="Add">
+</form>
+`
